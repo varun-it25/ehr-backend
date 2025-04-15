3e84ca8b94b06737347785f1952d8585b80f9389
varun kangotra <varunkangotra.it25@gmail.com>
2025-04-15 10:49:41 +0530

    Correct for vercel

--- controllers/billing.controller.js
+++
@@ -1,4 +0,0 @@
-export async function createBill(){}
-export async function updateBill(){}
-export async function readBill(){}
-export async function deleteBill(){}
--- controllers/medicine.controller.js
+++
@@ -1,4 +0,0 @@
-export async function createMedicine(){}
-export async function updateMedicine(){}
-export async function readMedicine(){}
-export async function deleteMedicine(){}
