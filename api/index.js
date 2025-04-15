43c138a4e27f082f8f4c8aeb49e5a67a7b4b829d
varun kangotra <varunkangotra.it25@gmail.com>
2025-04-15 10:40:34 +0530

    Folder updated for Vercel

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import cors from "cors";
+import express from "express";
+import billingRoutes from "../routes/billing.route.js"
+import patientRoutes from "../routes/patient.route.js"
+import medicineRoutes from "../routes/medicine.route.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/patient", patientRoutes);
+app.use("/medicine", medicineRoutes);
+app.use("/billing", billingRoutes);
+
+app.get('/',(req, res) => {
+    res.status(200).send('Welcome to EHR Backend')
+})
+
+app.listen(3000, () => {
+    console.log('Server runs on PORT 3000.');
+});
+
+export default function handler(req, res) {
+    const parsedUrl = parse(req.url, true);
+    app(req, res);
+}
